6c83d90617ed865ce99163391c8f75ba568b5240
Sebastian Villegas Zapata <svillegasz@psl.com.co>
2017-09-25 15:54:26 -0500

    Github Api: Gist create and delete

---
+++ test/GithubApi.Gist.test.js
@@ -0,0 +1,63 @@
+const agent = require('superagent-promise')(require('superagent'), Promise);
+const statusCode = require('http-status-codes');
+const { expect, assert } = require('chai');
+
+const urlBase = 'https://api.github.com';
+
+describe('Github Api Test', () => {
+  describe('Gists', () => {
+    let response;
+    let issue;
+
+    const body = {
+      description: 'the description for this gist',
+      public: true,
+      files: {
+        'file1.txt': {
+          content: 'String file contents'
+        }
+      }
+    };
+
+    beforeEach(() =>
+      agent.post(`${urlBase}/gists`)
+        .auth('token', process.env.ACCESS_TOKEN)
+        .send(body)
+        .then((res) => {
+          response = res;
+          issue = res.body;
+        }));
+
+    it('Should be created', () => {
+      expect(response.status).to.equal(statusCode.CREATED);
+      expect(issue).to.containSubset(body);
+    });
+
+    it('Should exists', () =>
+      agent.get(issue.url)
+        .auth('token', process.env.ACCESS_TOKEN)
+        .then((res) => {
+          expect(res.status).to.equal(statusCode.OK);
+        }));
+
+    describe('On delete', () => {
+      before(() =>
+        agent.del(issue.url)
+          .auth('token', process.env.ACCESS_TOKEN)
+          .then((res) => {
+            response = res;
+          }));
+
+      it('Should be deleted', () => {
+        expect(response.status).to.equal(statusCode.NO_CONTENT);
+      });
+
+      it('Should NOT exists', () =>
+        agent.get(issue.url)
+          .auth('token', process.env.ACCESS_TOKEN)
+          .then((res) => {
+            expect(res.status).to.equal(statusCode.NOT_FOUND);
+          }));
+    });
+  });
+});
