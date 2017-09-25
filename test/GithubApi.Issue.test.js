4261a82b2e7df1a030f3d94a13507f53d911da74
Sebastian Villegas Zapata <svillegasz@psl.com.co>
2017-09-25 14:47:44 -0500

    Github API: Issue create and update Tests

--- test/GithubApi.Issue.test.js
+++ test/GithubApi.Issue.test.js
@@ -1,25 +1,65 @@
 const agent = require('superagent-promise')(require('superagent'), Promise);
-const { expect } = require('chai');
+const { expect, assert } = require('chai');
 
 const urlBase = 'https://api.github.com';
-const githubUserName = 'svillegasz';
-const repository = 'psl-workshop-api-testing-js';
 
 describe('Github Api Test', () => {
-  describe('Issue', () => {
-    it('Should have a public repository', () =>
-      agent.get(`${urlBase}/user`)
-        .auth('token', process.env.ACCESS_TOKEN)
-        .then((response) => {
-          expect(response.body.public_repos).to.be.at.least(1);
-        }));
+  describe('User', () => {
+    let user;
 
-    it('Via OAuth2 Tokens by parameter', () =>
-      agent.get(`${urlBase}/repos/${githubUserName}/${repository}`)
-        .query(`access_token=${process.env.ACCESS_TOKEN}`)
-        .then((response) => {
-          expect(response.status).to.equal(statusCode.OK);
-          expect(response.body.description).equal('This is a Workshop about Api Testing in JavaScript');
-        }));
+    before(() => agent.get(`${urlBase}/user`)
+      .auth('token', process.env.ACCESS_TOKEN)
+      .then((response) => {
+        user = response.body;
+      }));
+
+    it('Should have a public repository', () => {
+      expect(user.public_repos).to.be.at.least(1);
+    });
+
+    describe('Repository', () => {
+      let repository;
+      before(() =>
+        agent.get(user.repos_url)
+          .auth('token', process.env.ACCESS_TOKEN)
+          .then((response) => {
+            repository = response.body.find(repo => repo.name === 'frisby-tutorial');
+          }));
+
+      it('Should be valid', () => {
+        assert.isOk(repository);
+      });
+
+      describe('Issue', () => {
+        let issue;
+
+        before(() =>
+          agent
+            .post(`${repository.url}/issues`)
+            .auth('token', process.env.ACCESS_TOKEN)
+            .send({
+              title: 'Testing the Issue creation!'
+            })
+            .then((response) => {
+              issue = response.body;
+            }));
+
+        it('Should create an Issue', () => {
+          expect(issue.title).to.equal('Testing the Issue creation!');
+        });
+
+        it('Should update an Issue', () =>
+          agent
+            .patch(issue.url)
+            .auth('token', process.env.ACCESS_TOKEN)
+            .send({
+              body: 'This is an awesome issue!'
+            })
+            .then((response) => {
+              expect(response.body.title).to.equal('Testing the Issue creation!');
+              expect(response.body.body).to.equal('This is an awesome issue!');
+            }));
+      });
+    });
   });
 });
