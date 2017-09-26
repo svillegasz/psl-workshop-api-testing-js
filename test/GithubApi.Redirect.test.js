70c4d92621753e6a48db08ea2c584512efa0296f
svillegasz <30663053+svillegasz@users.noreply.github.com>
2017-09-26 16:19:11 -0500

    GitHub head (#11)
    * Github API: Redirect
    
    * Github API: Redirect Test
    
    * Desc only removed

---
+++ test/GithubApi.Redirect.test.js
@@ -0,0 +1,30 @@
+const agent = require('superagent-promise')(require('superagent'), Promise);
+const statusCode = require('http-status-codes');
+const { expect, assert } = require('chai');
+
+const urlBase = 'https://github.com';
+const githubUserName = 'aperdomob';
+
+describe('Github Api Test', () => {
+  describe('HEAD Service', () => {
+    let res;
+
+    before(() =>
+      agent.head(`${urlBase}/${githubUserName}/redirect-test`)
+        .catch((response) => {
+          res = response.response;
+        }));
+
+    it('Should respond with a redirect', () => {
+      expect(res.status).to.equal(statusCode.MOVED_PERMANENTLY);
+      expect(res.headers.location).to.equal('https://github.com/aperdomob/new-redirect-test');
+    });
+
+    it('Should redirect on GET request', () =>
+      agent.get(`${urlBase}/${githubUserName}/redirect-test`)
+        .then((response) => {
+          expect(response.status).to.equal(statusCode.OK);
+          expect(response.type).to.equal('text/html');
+        }));
+  });
+});
