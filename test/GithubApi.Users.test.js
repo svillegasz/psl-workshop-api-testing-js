8b7549efa62ac6ff87490d274d25451dd0408c6a
svillegasz <30663053+svillegasz@users.noreply.github.com>
2017-09-26 16:55:44 -0500

    GitHub query (#12)
    * Github API: query params tests
    
    * Test messages updated
    
    * Test messages updated
    
    * Desc only removed

---
+++ test/GithubApi.Users.test.js
@@ -0,0 +1,36 @@
+const agent = require('superagent-promise')(require('superagent'), Promise);
+const statusCode = require('http-status-codes');
+const { expect } = require('chai');
+
+const urlBase = 'https://api.github.com';
+
+describe('Github Api Test', () => {
+  describe('Query params', () => {
+    it('Should return 30 Users by default', () =>
+      agent.get(`${urlBase}/users`)
+        .then((response) => {
+          expect(response.status).to.equal(statusCode.OK);
+          expect(response.body.length).to.equal(30);
+        }));
+
+    it('Should return 10 Users', () => {
+      const limit = 10;
+      return agent.get(`${urlBase}/users`)
+        .query(`per_page=${limit}`)
+        .then((response) => {
+          expect(response.status).to.equal(statusCode.OK);
+          expect(response.body.length).to.equal(limit);
+        });
+    });
+
+    it('Should return 50 users', () => {
+      const limit = 50;
+      return agent.get(`${urlBase}/users`)
+        .query(`per_page=${limit}`)
+        .then((response) => {
+          expect(response.status).to.equal(statusCode.OK);
+          expect(response.body.length).to.equal(limit);
+        });
+    });
+  });
+});
