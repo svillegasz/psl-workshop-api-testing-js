ab24b8c248504fc53bb75c5670e447000baf39b0
svillegasz <30663053+svillegasz@users.noreply.github.com>
2017-09-27 09:16:09 -0500

    GitHub res time (#13)
    * Github API: query params tests
    
    * Test messages updated
    
    * Test messages updated
    
    * Github API: Response Time Test
    
    * Auth added to all tests
    
    * Fix: unused auth deleted

--- test/GithubApi.Users.test.js
+++ test/GithubApi.Users.test.js
@@ -1,4 +1,5 @@
 const agent = require('superagent-promise')(require('superagent'), Promise);
+const responseTime = require('superagent-response-time');
 const statusCode = require('http-status-codes');
 const { expect } = require('chai');
 
@@ -6,8 +7,16 @@ const urlBase = 'https://api.github.com';
 
 describe('Github Api Test', () => {
   describe('Query params', () => {
+    it('Should respond in less than 5 seconds', () =>
+      agent.get(`${urlBase}/users`)
+        .auth('token', process.env.ACCESS_TOKEN)
+        .use(responseTime((req, time) => {
+          expect(time).to.be.at.most(5000);
+        })));
+
     it('Should return 30 Users by default', () =>
       agent.get(`${urlBase}/users`)
+        .auth('token', process.env.ACCESS_TOKEN)
         .then((response) => {
           expect(response.status).to.equal(statusCode.OK);
           expect(response.body.length).to.equal(30);
@@ -16,6 +25,7 @@ describe('Github Api Test', () => {
     it('Should return 10 Users', () => {
       const limit = 10;
       return agent.get(`${urlBase}/users`)
+        .auth('token', process.env.ACCESS_TOKEN)
         .query(`per_page=${limit}`)
         .then((response) => {
           expect(response.status).to.equal(statusCode.OK);
@@ -26,6 +36,7 @@ describe('Github Api Test', () => {
     it('Should return 50 users', () => {
       const limit = 50;
       return agent.get(`${urlBase}/users`)
+        .auth('token', process.env.ACCESS_TOKEN)
         .query(`per_page=${limit}`)
         .then((response) => {
           expect(response.status).to.equal(statusCode.OK);
